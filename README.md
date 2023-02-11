cddd9c3604af981a98df5a0ec8d3372f93cd9b8d
David Tuk <david@gohike.nl>
2023-02-11 23:39:26 +0100

    edits because of backend changes

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Create your own, shared list with Untappd beers.
 
-v0.1: do a search, save it to a list in localStorage.
-
 to do:
 
 - save result to headless db
--- src/components/DbGetTest.js
+++ src/components/DbGetTest.js
@@ -5,8 +5,8 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 const DbGetTest = () => {
   const fetcherOptions = { revalidateOnFocus: false };
 
-  const { data, isLoading } = useSWR(
-    'http://localhost:3003/users',
+  const { data: users, isLoading } = useSWR(
+    'http://localhost:3003/beers',
     fetcher,
     fetcherOptions
   );
@@ -14,8 +14,33 @@ const DbGetTest = () => {
     return <div>Loading...</div>;
   }
   // eslint-disable-next-line no-console
-  console.log(data);
-  return <div>hoi</div>;
+  // console.log(users);
+
+  return (
+    <table className="table is-striped is-fullwidth">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Name</th>
+          <th>k</th>
+          <th>k</th>
+          <th>k</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user, index) => (
+          <tr key={user.id}>
+            <td>{index + 1}</td>
+            <td>{user.name}</td>
+            <td>{user.brewery_name}</td>
+            <td>{user.rating}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  // return <div>hoi</div>;
 };
 
 export { DbGetTest };
